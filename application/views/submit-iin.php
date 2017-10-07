8d055d260f34532b5d33f54e7cf0a7c4eaa8c8d7
akhmadandaru <31055871+akhmadandaru@users.noreply.github.com>
2017-10-08 02:22:48 +0700

    Penambahan Vali dasi blm kelar

--- application/views/submit-iin.php
+++ application/views/submit-iin.php
@@ -5,11 +5,14 @@
 </div>
 
 
+
 <div class="page" style="margin-top: 150px">
 	<div class="clearfix">
 		<ul id="section_progress" style="width: 100%">
-			<li stepId="0" class="COMPLETED"><button>Pengajuan Surat Permohonan ke BSN</button></li>
-			<li stepId="1" class="COMPLETED"><button>Hasil Verifikasi Status Permohonan</button></li>
+			<li stepId="0" class="<?php $Status ; $Status= $this->session->flashdata('satu') == "COMPLETED" ? "COMPLETED": (  $this->session->flashdata('satu') == "PENDING"  ? 'PENDING' : ''); echo $Status ?>"><button>Pengajuan Surat Permohonan ke BSN</button></li>
+			<li stepId="1" class="<?php $Status ; $Status= $this->session->flashdata('dua') == "COMPLETED" ? "PENDING": (  $this->session->flashdata('dua') == "COMPLETED"
+					? 'COMPLETED' : ''
+				   ); echo $Status ?>"><button>Hasil Verifikasi Status Permohonan</button></li>
 			<li stepId="2" class="COMPLETED"><button>Submit Kelengkapan Dokumen</button></li>
 			<li stepId="3" class="PENDING"><button>Proses Verifikasi dan Validasi</button></li>
 			<li stepId="4" class=""><button>Konfirmasi Surat Lulus Kelengkapan dan Kode Billing</button></li>
@@ -22,22 +25,23 @@
 
 
 		<?php 
-		$id_user = $this->session->userdata('id_user');
-	
-	if ($this->user_model->getdocument_aplication($id_user) ){
-
-		$data['download_upload']    = $this->user_model->getdocument_aplication($id_user);
-
-		$datas['aplication_asesment']    = $this->user_model->getAssesmentStatus($id_user);
-	}
-
-
-	// if ($this->user_model->getAplicationStatus($id_user) ){
-
+// echo $this->session->flashdata('1');
 		
-	// }
+		if($this->session->userdata('status') != "login"){
+			redirect(base_url("SipinHome"));
+		}
 
+		$id_user = $this->session->userdata('id_user');	
+		$Status =  $this->user_model->get_applications_Status($id_user);
+		$data['download_upload']    = $this->user_model->getdocument_aplication($id_user);
+		$datas['aplication_asesment']    = $this->user_model->getAssesmentStatus($id_user);
+
+
+
+ 	
+		
 		$this->load->view('submitIIN/step0');
+
 		$this->load->view('submitIIN/step1',$data); 
 		$this->load->view('submitIIN/step2',$data);
 		$this->load->view('submitIIN/step3',$data);	
@@ -47,6 +51,7 @@
 		$this->load->view('submitIIN/step7',$data);
 		$this->load->view('submitIIN/step8',$data);
 		$this->load->view('submitIIN/step9',$data);
+
 ?>
 	</div>
 </div>
