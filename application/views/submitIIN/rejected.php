6337e22f6c9b6707eae38df1c5cdeb65d6825195
novalenp <novalenp@gmail.com>
2017-10-22 16:06:11 +0700

    process - rejected
    minor changes

--- application/views/submitIIN/rejected.php
+++ application/views/submitIIN/rejected.php
@@ -1,6 +1,8 @@
 <section section-id="rejected" class="section_iin float_right" style="display:none">
 	<h1 class="title_iin"><?php echo $rejected_view['title'] ?></h1>
-	<p class="step-process"><?php echo $rejected_view['text'] ?>
+	<p class="step-process"><?php echo $rejected_view['text'] ?></p>
+	<br>
+	<center>
 		<a href="<?php echo base_url() ?>"><button id="btn_back" style="background:red" class="btn_back">Kembali ke Halaman Depan</button></a>
-	</p>
+	</center>
 </section>
