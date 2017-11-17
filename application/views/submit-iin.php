eac4c9313b1d60a7734185a56fa4498a778e611c
novalenp <novalenp@gmail.com>
2017-11-17 14:23:44 +0700

    submit_iin

--- application/views/submit-iin.php
+++ application/views/submit-iin.php
@@ -125,7 +125,7 @@
 		);
 		$this->load->view('submitIIN/rejected', $data);
 
-		$this->load->view('submitIIN/step0-rejected', $data);
+		// $this->load->view('submitIIN/step0-rejected', $data);
 
 		// REVISION VIEW
 		// $this->load->view('submitIIN/step2-revision');
