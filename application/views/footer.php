e3e69df43114deea515aaf31069141c459c7e778
samRoxao <rinaldysam@codysseia.com>
2017-11-24 02:33:20 +0700

    Update Home
    = Banner menjadi Effect FadeIn(), FadeOut()
    + Footer Responsive UI,
    + Penambahan Button Download pada View iin-list-view
    + User_model Penambahan Query dengan nama get_iin_download

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,34 +1,36 @@
 <footer>
-		<div class="footer_container clearfix">
-			<div class="float_left footer_content_left">
-				<div><img src="<?php echo base_url() ?>assets/logo.png" alt="" width="200px"></div>
-				<div class="footer_copyright">Hak Cipta 2016 © Standardisasi Nasional</div>
-			</div>
-			<div class="float_right footer_content_right">
-				<div class="footer_socmed">
-					<svg class="sprite_icon" fill="#bb0000"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_yt"/></svg>
-					<svg class="sprite_icon" fill="#00aced"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_tw"/></svg>
-					<svg class="sprite_icon" fill="#3b5998"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_fb"/></svg>
-					<svg class="sprite_icon" fill="#bc2a8d"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_ig"/></svg>
+	<div class="footer_container">
+		<div class="footer_address">
+			<div class="footer-contact-list">
+				<div class="footer-item-address">
+					<img src="<?=base_url('assets/location.svg')?>">
+					<div>Pusat Pendidikan dan Pemasyarakatan Standardisasi BSN<br/>
+						Gedung I BPPT Lt. 10, Jalan M.H. Thamrin No. 8 <br/>
+						Jakarta Pusat, 10340
+					</div>
 				</div>
-				<div class="footer_contact_list">
-					<div><svg class="sprite_icon float_left" fill="#555"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_address"/></svg>
-						<div>Pusat Pendidikan dan Pemasyarakatan Standardisasi BSN<br/>
-							Gedung I BPPT Lt. 10<br/>
-							Jalan M.H. Thamrin No. 8, <br/>
-							Jakarta Pusat, 10340
-						</div>
-					</div>
-					<div><svg class="sprite_icon float_left" fill="#555"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_phone"/></svg>
-						<div>021 1234 1234 - 021 1234 1234</div>
-					</div>
-					<div><svg class="sprite_icon float_left" fill="#555"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_mail"/></svg>
-						<div>kerjasama@bsn.go.id</div>
-					</div>
-					
+				<div class="footer-item-phone">
+					<img src="<?=base_url('assets/phone.svg')?>">
+					<div>021 1234 1234 - 021 1234 1234</div>
 				</div>
+				<div class="footer-item-mail">
+					<img src="<?=base_url('assets/email.svg')?>">
+					<div>kerjasama@bsn.go.id</div>
+				</div>
+				
 			</div>
-		</div>	
-	</footer>
+		</div>
+		<div class="footer_socmed">
+			<svg class="sprite_icon" fill="#bb0000"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_yt"/></svg>
+			<svg class="sprite_icon" fill="#00aced"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_tw"/></svg>
+			<svg class="sprite_icon" fill="#3b5998"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_fb"/></svg>
+			<svg class="sprite_icon" fill="#bc2a8d"><use xlink:href="<?php echo base_url()?>assets/ic_socmed.svg#ic_ig"/></svg>
+		</div>
+		<div class="footer_copyright">
+			<div><img src="<?php echo base_url() ?>assets/logo.png" alt=""></div>
+			<div class="footer_copyright">Hak Cipta 2017 © Standardisasi Nasional</div>
+		</div>
+	</div>	
+</footer>
 
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/ui.min.js"></script>
