6a50c16516e219a5adc1384bdc98b4714158cf81
samRoxao <rinaldysam@codysseia.com>
2017-11-25 00:23:41 +0700

    Survey View
    OK

--- application/views/survey-result.php
+++ application/views/survey-result.php
@@ -1,23 +1,31 @@
 
-    <section id="cms-section" class="clearfix sheets_paper">
+    
+    <section id="cms-section" class="survey-section clearfix sheets_paper">
       <div class="cms-header">
         <span class="cms-header-date"><a href="<?=base_url()?>" style="color:#3b9f58">HOME</a> >Hasil SURVEI</span>
         <h1 class="cms-header-title">HASIL SURVEI</h1>  
       </div>
-      <article style="max-width: 1000px">
+      <article style="max-width: 900px">
           <div class="survey-header">
             <div class="id_no">No.</div>
             <div class="id_qs">Pertanyaan</div>
             <div class="id_as">Nilai</div>
           </div>
-          <?php for ($i=0; $i < count($survey['survey_questions']) ; $i++):$x=$survey['survey_questions'][$i]?>
+          <?php for ($i=0; $i < count($survey['survey_questions']) ; $i++):
+                $x=$survey['survey_questions'][$i]?>
           <div class="survey-row">
             <div class="id_no"> <?= $x['no'] ?> </div>
             <div class="id_qs"> <?= $x['question'] ?> </div>
             <div class="id_as td-stars"> 
-              <?php for ($j=0;$j < round($x['average']) ; $j++): echo "<span></span>";endfor ?>
+              <div class="c-rating_no"><span><?=number_format($x['average'],1)?></span>/5</div>
+              <div class="c-rating">
+                <div class="c-rating_bg"></div>
+                <div class="c-rating_fg" style="width: <?=((100/5)*$x['average'])?>%"></div>
+              </div>
+              <div class="c-rating_pv"><?=$survey['participant']?> Partisipan</div>
             </div>
           </div>
           <?php endfor ?>
       </article>
     </section>
+
